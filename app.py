919729b3be6307ee2501d472af826e4c9e0b97f1
Edwar1007 <edwar.lopez@sutex.com>
2025-05-22 09:25:04 -0500

    app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def home():
 def todos_equipos():
     try:
         inicio = int(request.args.get("inicio", 0))
-        cantidad = int(request.args.get("cantidad", 120))
+        cantidad = int(request.args.get("cantidad", 100))
     except ValueError:
         return jsonify({"error": "Parámetros 'inicio' y 'cantidad' deben ser enteros"}), 400
 
